2dc1682c581287518afba99f7779368904f91e8f
Atlas Cove <Atlas48@gmx.com>
2023-02-05 19:00:54 +0000

    Initial commit.

---
+++ index.js
@@ -0,0 +1,23 @@
+import reorg from '@orgajs/reorg'
+import stream from 'unified-stream'
+import mutate from '@orgajs/reorg-rehype'
+import html from 'rehype-stringify'
+import {Readable} from 'stream'
+
+const processor = reorg().use(mutate).use(html)
+
+//https://stackoverflow.com/questions/10623798/how-do-i-read-the-contents-of-a-node-js-stream-into-a-string-variable#49428486
+function streamToString (stream) {
+    const chunks = [];
+    return new Promise((resolve, reject) => {
+      stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
+      stream.on('error', (err) => reject(err));
+      stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
+    })
+  }
+
+export default async function org_html(instr) {
+    var buf = new Readable
+    buf.push(instr)
+    return await streamToString(buf.pipe(stream(processor)))
+}
