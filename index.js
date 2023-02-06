1ab95199d60e099e052cfb21471b74d1e490677b
Atlas Cove <Atlas48@gmx.com>
2023-02-06 14:01:29 +0000

    function re-jig

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-import reorg from '@orgajs/reorg'
-import {stream} from 'unified-stream'
-import mutate from '@orgajs/reorg-rehype'
-import html from 'rehype-stringify'
-import {Readable} from 'stream'
-
-const processor = reorg().use(mutate).use(html)
-
-//https://stackoverflow.com/questions/10623798/how-do-i-read-the-contents-of-a-node-js-stream-into-a-string-variable#49428486
-function streamToString (stream) {
-    const chunks = [];
-    return new Promise((resolve, reject) => {
-      stream.on('data', (chunk) => chunks.push(Buffer.from(chunk)));
-      stream.on('error', reject);
-      stream.on('end', () => resolve(Buffer.concat(chunks).toString('utf8')));
-    })
-  }
+import unified from 'unified'
+import reorg from '@orgajs/reorg-parse'
+import mkast from '@orgajs/reorg-rehype'
+import stringify from 'rehype-stringify'
 
 export default async function org_html(instr) {
-    var buf = new Readable
-    buf._read ??= ()=>{} //probably not a good idea but eh
-    buf.push(instr)
-    buf.push(null)
-    return await streamToString(buf.pipe(stream(processor)))
+  const out = await unified().use(reorg).use(mkast).use(stringify).process(instr)
+  return String(out)
 }
